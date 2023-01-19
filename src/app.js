ee16fec9f0cc42612b5f9118c557bcb96696d548
Jonas0Dias <jonaaasdiaaas@gmail.com>
2023-01-19 02:05:02 -0300

    encrypt password

--- src/app.js
+++ src/app.js
@@ -35,12 +35,18 @@ const signUpValidation = Joi.object({
     passwordConfirmation: Joi.string().valid(Joi.ref("password")).required()
   });
 
+const newEntryOrExit = Joi.object({
+    value: Joi.number().required(),
+    description: Joi.string().required(),
+  });
+
 
 
 
 // ROTAS:
 
 app.post('/login', async (req, res) => {
+    console.log('teste')
   const user = req.body
   const validate = loginValidation.validate(user, { abortEarly: true })
 
@@ -64,6 +70,7 @@ app.post('/login', async (req, res) => {
 
 app.post('/signup', async (req, res) => {
     const user = req.body;
+    const passwordHash = bcrypt.hashSync(user.password, 5);
     const validate = signUpValidation.validate(user, { abortEarly: true });
     if (validate.error) {
         res.status(422).send(validate.error);
@@ -75,13 +82,53 @@ app.post('/signup', async (req, res) => {
             res.status(409).send('Este email já foi cadastrado anteriormente')
             return;
         }  
-        await db.collection('users').insertOne(user);
+        await db.collection('users').insertOne({email:user.email, name: user.name, password: passwordHash});
         res.status(201).send('Usuário cadastrado com sucesso')
     }
     catch(err){
         console.log(err);
     }
   })
+
+app.get('/home',async(req,res) => {
+    const expensesList = await db.collection("expenses").find().toArray()
+    res.send(expensesList)
+})
+
+app.post('/newentry',async(req,res) => {
+    const entry = req.body; //espero q o expenses seja um objeto com valor, descrição e data no formato DD/MM
+    const validate = newEntryOrExit.validate(entry, { abortEarly: true });
+    try{
+        if (validate.error) {
+            res.status(422).send(validate.error);
+            return;
+        }
+        await db.collection('entry').insertOne(entry);
+        res.status(201).send('Entrada cadastrada com sucesso!')
+        }
+    catch(err){
+        res.send(err)
+    }
+}
+) 
+
+app.post('/newexit',async(req,res) => {
+    const exit = req.body; //espero q o expenses seja um objeto com valor, descrição e data no formato DD/MM
+    const validate = newEntryOrExit.validate(entry, { abortEarly: true });
+    try{
+        if (validate.error) {
+            res.status(422).send(validate.error);
+            return;
+        }
+        await db.collection('exit').insertOne(exit);
+        res.status(201).send('Gasto cadastrado com sucesso!')
+        }
+    catch(err){
+        res.send(err)
+    }
+}
+) 
+
 
 const port = 5000;
 app.listen(port, () => console.log(`Server running in port: ${port}`));
